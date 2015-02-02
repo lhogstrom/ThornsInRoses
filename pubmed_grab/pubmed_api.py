7dab244d567ae17e1efac53857be1cc375c984a9
Larson Hogstrom <hogstrom@broadinstitute.org>
2015-02-02 12:24:45 -0500

    pumed api scrape

--- pubmed_grab/pubmed_api.py
+++ pubmed_grab/pubmed_api.py
@@ -1,6 +1,7 @@
 from Bio import Entrez
+import numpy as np
 ### pull info from pubmed api
-
+outDir = '/Users/hogstrom/Dropbox (MIT)/pubmed_scrp'
 
 ### efetch example
 Entrez.efetch(id="57240072")
@@ -34,10 +35,53 @@ print record[0]["Id"]
 
 ### search for specific email in abstract
 # handle = Entrez.efetch(db="pubmed", id="25081398", rettype="abstract")
-handle = Entrez.esearch(db="pubmed", rettype="abstract", term="pfizer.com",retmax=10)
+# search_term = ".com&Cambridge, MA"
+# search_term = ".com&Boston"
+# search_term = "Novartis&Cambridge, MA"
+# oFile = outDir + '/Novartis_tbl.txt'
+search_term = "oncotype dx"
+oFile = outDir + '/breast_cancer_tbl.txt'
+handle = Entrez.esearch(db="pubmed", rettype="abstract", term=search_term,retmax=100)
+# handle = Entrez.esearch(db="pubmed", rettype="abstract", term="Cambridge, MA",retmax=10)
 record = Entrez.read(handle)
 handle.close()
+idList = record['IdList']
 
+### retrieve article info
+handle = Entrez.efetch(db="pubmed", id=idList, rettype="abstract")
+abstract_records = Entrez.read(handle)
+handle.close()
 
-### desired out-put
-#author, email, journal article, title, date, location
+#loop through for each - save article info
+articles_dict = {}
+for a_rec in abstract_records:
+    article_dict = {}
+    # title
+    article_dict['journal'] = a_rec['MedlineCitation']['Article']['Journal']['Title']
+    # date
+    date_entry = a_rec['MedlineCitation']['Article']['ArticleDate']
+    if date_entry:
+        article_dict['Date'] = date_entry[0]['Year']
+    # title
+    art_title = a_rec['MedlineCitation']['Article']['ArticleTitle']
+    art_title = ''.join([i if ord(i) < 128 else ' ' for i in art_title]) #remove non ascii
+    art_title = art_title.encode('utf-8') 
+    article_dict['Title'] = art_title
+    # authors
+    authorList = a_rec['MedlineCitation']['Article']['AuthorList']
+    author_number = len(authorList)
+    article_dict['last_author'] = authorList[-1]['LastName']
+    if authorList[-1]['AffiliationInfo']: 
+        article_dict['last_author_affiliation'] = authorList[-1]['AffiliationInfo'][0]['Affiliation']
+    pID = a_rec['MedlineCitation']['PMID']
+    articles_dict[str(pID)] = article_dict
+aFrm = pd.DataFrame(articles_dict)
+aFrm = aFrm.T # transpose
+# save file
+aFrm.to_csv(oFile,sep='\t')
+
+# journal_name = record[0]['MedlineCitation']['Article']['Journal']['Title']
+# abstract_text = record[0]['MedlineCitation']['Article']['Abstract']
+
+### desired out-pu
+#author, email, journal article, title, date, location, pubmed_id
