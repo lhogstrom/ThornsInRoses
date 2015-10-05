016073240098312c3c0976d8e90db8949865b6ea
Larson Hogstrom <hogstrom@mit.edu>
2015-10-05 11:06:07 -0400

    test commit1

--- dsde/bam_sampling.sh
+++ dsde/bam_sampling.sh
@@ -3,5 +3,5 @@
 ### write BAM index files to text
 file1 = '/seq/picard_aggregation/G76270/NA12878/current/NA12878.bam'
 
-# view file according to read group number
+# view file according to read group number - 
 samtools view -r H01PE.3 $file1
