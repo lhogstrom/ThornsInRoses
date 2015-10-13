d660f09ebf3098e559e45e11197c4c2c372be65f
Larson Hogstrom <hogstrom@mit.edu>
2015-10-13 16:13:34 -0400

    mark dup script update

--- dsde/read_grp_mark_dup.sh
+++ dsde/read_grp_mark_dup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-use Samtools 
+#use Samtools 
 
-export picard=/seq/software/picard/current/bin/picard.jar
-export outdir=/humgen/gsa-hpprojects/dev/hogstrom/depth_by_read_group
+picard=/seq/software/picard/current/bin/picard.jar
+outdir=/humgen/gsa-hpprojects/dev/hogstrom/depth_by_read_group
 
+java -jar $picard SortSam \
+    INPUT=$outdir/NexPond-376014_chr21_rg_H23N7-1.bam \
+    OUTPUT=$outdir/NexPond-376014_chr21_rg_H23N7-1_sorted.bam \
+    SORT_ORDER=coordinate 
 
-java -jar $picard MarkDuplicates \ 
-INPUT=$outdir/NexPond-376014_chr21_rg_H23N7-1.bam \ 
-OUTPUT=$outdir/NexPond-376014_chr21_rg_H23N7-1_dupMarked.bam \
-METRICS_FILE=$outdir/NexPond-376014_chr21_rg_H23N7-1_dup_Metrics.txt
-
+#java -jar $picard MarkDuplicates \
+#	INPUT=$outdir/NexPond-376014_chr21_rg_H23N7-1.bam \
+#	OUTPUT=$outdir/NexPond-376014_chr21_rg_H23N7-1_dupMarked.bam \
+#	METRICS_FILE=$outdir/NexPond-376014_chr21_rg_H23N7-1_dup_Metrics.txt
